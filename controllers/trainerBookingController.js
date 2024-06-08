c2382b670fc048f92838b380c73ed5ed79fa7662
chaudhuree <chaudhuree00@gmail.com>
2024-06-08 14:49:30 +0600

    trainer booking related controller and route added

---
+++ controllers/trainerBookingController.js
@@ -0,0 +1,153 @@
+import { StatusCodes } from "http-status-codes";
+import TrainerBooking from "../models/TrainerBooking";
+import User from "../models/User";
+
+/*sample data
+{
+  "trainer": "60e5f4b5c3e9d3b3f0f3b3b3",
+  "user": "60e5f4b5c3e9d3b3f0f3b3b3",
+  "date": "2021-07-08", 
+  "slotTime": {
+    "slotName": "morning",
+    "scheduleTime": "6.00 AM"
+  },
+  "package": "premium",
+  "status": "approved",
+  "traisactionId": "123456",
+  "price": 100
+}
+
+*/
+
+// add a new trainer booking
+const addTrainerBooking = async (req, res) => {
+  try {
+    const trainerBookingInstance = await TrainerBooking.create(req.body);
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      data: trainerBookingInstance,
+      message: "Trainer booking added successfully",
+    });
+  } catch (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: error.message });
+  }
+};
+
+// get all trainer bookings with pagination and sorting and filtering using trainer id
+const getAllTrainerBookings = async (req, res) => {
+  const { page = 1, limit = 10, trainerId } = req.query;
+  try {
+    if (trainerId) {
+      const trainerBookings = await TrainerBooking.find({ trainer: trainerId })
+        .populate("user")
+        .populate("trainer")
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      const count = await TrainerBooking.countDocuments({ trainer: trainerId });
+      res.status(StatusCodes.OK).json({
+        success: true,
+        data: { trainerBookings, total: count },
+        message: "Trainer bookings retrieved successfully",
+      });
+    } else {
+      const trainerBookings = await TrainerBooking.find()
+        .populate("user")
+        .populate("trainer")
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      const count = await TrainerBooking.countDocuments();
+      res.status(StatusCodes.OK).json({
+        success: true,
+        data: { trainerBookings, total: count },
+        message: "Trainer bookings retrieved successfully",
+      });
+    }
+  } catch (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: error.message });
+  }
+};
+
+// get all trainer bookings with pagination and sorting and filtering using user name
+
+const getAllUserBookings = async (req, res) => {
+  const { page = 1, limit = 10, userName } = req.query;
+  try {
+    const userId = await User.findOne({
+      displayName: userName,
+    }).select("_id");
+    if (userId) {
+      const userBookings = await TrainerBooking.find({ user: userId })
+        .populate("user")
+        .populate("trainer")
+        .limit(limit * 1)
+        .skip((page - 1) * limit);
+      const count = await TrainerBooking.countDocuments({ user: userId });
+      res.status(StatusCodes.OK).json({
+        success: true,
+        data: { userBookings, total: count },
+        message: "User bookings retrieved successfully",
+      });
+    } else {
+      res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+  } catch (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: error.message });
+  }
+};
+
+// get all data of using trainer and date and slot time
+const getTrainerBookingByDate = async (req, res) => {
+  const { trainerId, date, scheduleTime } = req.body;
+  try {
+    if (scheduleTime) {
+      const trainerBookings = await TrainerBooking.find({
+        trainer: trainerId,
+        date,
+        'slotTime.scheduleTime': scheduleTime,
+      })
+        .populate("user")
+        .populate("trainer");
+      res.status(StatusCodes.OK).json({
+        success: true,
+        data: trainerBookings,
+        message: "Trainer bookings retrieved successfully",
+      });
+    } else {
+      const trainerBookings = await TrainerBooking.find({
+        trainer: trainerId,
+        date,
+      });
+      res.status(StatusCodes.OK).json({
+        success: true,
+        data: trainerBookings,
+        message: "Trainer bookings retrieved successfully",
+      });
+    }
+  } catch (error) {
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ success: false, message: error.message });
+  }
+};
+// sample data
+// {
+//   "trainerId": "60e5f4b5c3e9d3b3f0f3b3b3",
+//   "date": "2021-07-08",
+//     "scheduleTime": "6.00 AM"
+// }
+
+module.exports = {
+  addTrainerBooking,
+  getAllTrainerBookings,
+  getAllUserBookings,
+  getTrainerBookingByDate,
+};
---
+++ routes/trainerBooking.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  addTrainerBooking,
+  getAllTrainerBookings,
+  getAllUserBookings,
+  getTrainerBookingByDate,
+} = require("../controllers/trainerBookingController");
+
+router.post("/trainerbooking/add", addTrainerBooking);
+router.get("/trainerbooking/all", getAllTrainerBookings);
+// url: http://localhost:5000/api/v1/trainerbooking/all?trainerId=60e5f4b5c3e9d3b3f0f3b3b3?page=1&limit=10
+router.get("/trainerbooking/usersbookings", getAllUserBookings);
+// url: http://localhost:5000/api/v1/trainerbooking/usersbookings?userName=John?page=1&limit=10
+router.post("/trainerbooking/trainers", getTrainerBookingByDate);
+// url: http://localhost:5000/api/v1/trainerbooking/trainers?date=2021-07-08?page=1&limit=10
+
+module.exports = router;
